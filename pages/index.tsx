ab8c2883c6772394154b044e7ee65ab785f81a08
notpankaj <iampankaj0409@gmail.com>
2022-04-23 11:58:02 +0530

    day-2 finish

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>NFT challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+        <h1 className='text-4xl font-bold text-red-500' > Welcome to the  NFT DROP CHALLENGE</h1>
+    
+    </div>
+  )
+}
+
+export default Home
